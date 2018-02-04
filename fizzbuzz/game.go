ebbfc89a3eca1bb76c7603290a3f705eba4432ec
derailed <fernand.galiana@gmail.com>
2018-02-04 09:16:43 -0700

    updates

--- fizzbuzz/game.go
+++ fizzbuzz/game.go
@@ -1,9 +1,6 @@
 // FizzBuzz game calculator
-package fizzbuzz
 
-import (
-	"fmt"
-)
+package fizzbuzz
 
 // compute returns a FizzBuzz number based on given inputs.
 // Returns `FizzBuzz` if number div by 3 and 5.
@@ -11,15 +8,5 @@ import (
 // Buzz if div by 5
 // Otherwise the given number
 func compute(n int) (s string) {
-	switch {
-	case n%3 == 0 && n%5 == 0:
-		s = "FizzBuzz"
-	case n%3 == 0:
-		s = "Fizz"
-	case n%5 == 0:
-		s = "Buzz"
-	default:
-		s = fmt.Sprintf("%d", n)
-	}
-	return
+	/// YOUR_CODE...
 }
--- fizzbuzz/game_test.go
+++ fizzbuzz/game_test.go
@@ -2,21 +2,8 @@ package fizzbuzz
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestCompute(t *testing.T) {
-	uc := map[int]string{
-		0:  "FizzBuzz",
-		1:  "1",
-		3:  "Fizz",
-		4:  "4",
-		5:  "Buzz",
-		15: "FizzBuzz",
-	}
-
-	for k, v := range uc {
-		assert.Equal(t, v, compute(k))
-	}
+	// YOUR_CODE...
 }
