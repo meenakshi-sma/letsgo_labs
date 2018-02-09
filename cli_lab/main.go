76d0ceba6fe38939f52ae3688ddd5f1a167ab374
derailed <fernand.galiana@gmail.com>
2018-02-09 16:11:32 -0700

    updates

--- cli_lab/main.go
+++ cli_lab/main.go
@@ -13,6 +13,8 @@ func main() {
 
 	flag.StringVar(&name, "u", "No one", "specifies a user name. Default: No one")
 	flag.IntVar(&age, "a", 42, "specifies user age. Default: 42")
+
+	// Don't Forget!
 	flag.Parse()
 
 	fmt.Printf("Hello my name is %s and I am %d years old!\n", name, age)
