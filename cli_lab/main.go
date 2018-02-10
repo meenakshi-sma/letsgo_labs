5993eadb1c87dcc00853344eb6ae09b90fd93b98
derailed <fernand.galiana@gmail.com>
2018-02-09 17:10:10 -0700

    updates

--- cli_lab/main.go
+++ cli_lab/main.go
@@ -1,19 +1,5 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-)
-
 func main() {
-	var (
-		name string
-		age  int
-	)
-
-	flag.StringVar(&name, "u", "No one", "specifies a user name. Default: No one")
-	flag.IntVar(&age, "a", 42, "specifies user age. Default: 42")
-	flag.Parse()
-
-	fmt.Printf("Hello my name is %s and I am %d years old!\n", name, age)
+	// YOUR_CODE...
 }
