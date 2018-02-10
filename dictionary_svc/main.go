f062a30d2cef5660c8c8da70e1f2c9c056431b86
derailed <fernand.galiana@gmail.com>
2018-02-10 12:19:19 -0700

    updates

--- dictionary_svc/main.go
+++ dictionary_svc/main.go
@@ -1,52 +1,24 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
-
-	"github.com/imhotepio/letsgo_labs/dictionary"
-	"github.com/imhotepio/letsgo_labs/jsondic"
 )
 
 const httpPort = ":4500"
 
 func main() {
-	setUpRoutes()
-
-	log.Printf("Private Dic is listening [%s]", httpPort)
-	http.ListenAndServe(httpPort, nil)
+	setupRoutes()
+	// YOUR_CODE...
 }
 
-func setUpRoutes() {
-	http.HandleFunc("/new_word", newWordHandler)
-	http.HandleFunc("/", noMatchHandler)
+func setupRoutes() {
+	// YOUR_CODE...
 }
 
 func newWordHandler(w http.ResponseWriter, r *http.Request) {
-	dic := r.URL.Query().Get("dic")
-
-	wl, err := dictionary.LoadDefault(dic)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusExpectationFailed)
-		return
-	}
-
-	buff, err := jsondic.Encode(jsondic.Entry{
-		Dictionary: dic,
-		Location:   "assets",
-		Word:       dictionary.Pick(wl),
-	})
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	fmt.Fprintf(w, buff)
-	log.Printf("[%d] %s", http.StatusOK, r.URL)
+	// YOUR_CODE...
 }
 
 func noMatchHandler(w http.ResponseWriter, r *http.Request) {
-	http.Error(w, fmt.Sprintf("No matching routes! %s", r.URL), http.StatusBadRequest)
+	// YOUR_CODE...
 }
