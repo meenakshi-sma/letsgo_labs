40fa914512b2b7b1ab6d75f597a81ba59158bea8
derailed <fernand.galiana@gmail.com>
2018-02-10 10:20:11 -0700

    updates

--- jsondic/dictionary_test.go
+++ jsondic/dictionary_test.go
@@ -2,37 +2,16 @@ package jsondic_test
 
 import (
 	"testing"
-
-	"github.com/imhotepio/letsgo_labs/jsondic"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestEncoding(t *testing.T) {
-	e := jsondic.Entry{
-		Dictionary: "artists",
-		Location:   "/tmp/assets",
-		Word:       "bumblebeetuna",
-	}
-	r, err := jsondic.Encode(e)
-
-	assert.Nil(t, err)
-	assert.Equal(t, r, `{"dictionary":"artists","location":"/tmp/assets","random_word":"bumblebeetuna"}`+"\n")
+	// YOUR_CODE...
 }
 
 func TestDecoding(t *testing.T) {
-	raw := `{"dictionary":"artists","location":"/tmp/assets","random_word":"bumblebeetuna"}`
-
-	e, err := jsondic.Decode(raw)
-
-	assert.Nil(t, err)
-	assert.Equal(t, "artists", e.Dictionary)
-	assert.Equal(t, "/tmp/assets", e.Location)
-	assert.Equal(t, "bumblebeetuna", e.Word)
+	// YOUR_CODE...
 }
 
 func TestDecodingFail(t *testing.T) {
-	raw := `fred`
-
-	_, err := jsondic.Decode(raw)
-	assert.NotNil(t, err)
+	// YOUR_CODE...
 }
