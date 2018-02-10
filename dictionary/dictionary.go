0b101bd0e94b80395f58bf71c533dee6af4da212
derailed <fernand.galiana@gmail.com>
2018-02-10 09:24:07 -0700

    updates

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -4,45 +4,19 @@
 // that can be randomly picked.
 package dictionary
 
-import (
-	"bufio"
-	"fmt"
-	"math/rand"
-	"os"
-	"path/filepath"
-	"time"
-)
-
-const rootDir = "./assets"
-
-// WordList a collection of dictionary words
-type WordList []string
-
-func init() {
-	rand.Seed(time.Now().UnixNano())
-}
+// YOUR_CODE...
 
 // Load loads a new dictionary given a base path and a dictionary name
 func Load(dir, name string) (wl WordList, e error) {
-	path := filepath.Join(dir, name+".txt")
-
-	if f, err := os.Open(path); err != nil {
-		e = fmt.Errorf("unable to load dictionary `%s", path)
-	} else {
-		sc := bufio.NewScanner(f)
-		for sc.Scan() {
-			wl = append(wl, sc.Text())
-		}
-	}
-	return
+	// YOUR_CODE...
 }
 
 // LoadDefault loads a dictionary by name from the default assets dir
 func LoadDefault(name string) (wl WordList, e error) {
-	return Load(rootDir, name)
+	// YOUR_CODE...
 }
 
 // Pick select a word at random from a list of words
 func Pick(l WordList) string {
-	return l[rand.Intn(len(l))]
+	// YOUR_CODE...
 }
--- dictionary/dictionary_test.go
+++ dictionary/dictionary_test.go
@@ -1,36 +1,21 @@
 package dictionary_test
 
 import (
-	"fmt"
 	"testing"
-
-	"github.com/imhotepio/letsgo_labs/dictionary"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestLoad(t *testing.T) {
-	l, err := dictionary.LoadDefault("musicians")
-
-	assert.Nil(t, err)
-	assert.Equal(t, 5, len(l))
+	// YOUR_CODE...
 }
 
 func TestPicker(t *testing.T) {
-	l, err := dictionary.LoadDefault("musicians")
-	w := dictionary.Pick(l)
-
-	assert.Nil(t, err)
-	assert.NotEqual(t, "", w)
+	// YOUR_CODE...
 }
 
 func TestLoadMissing(t *testing.T) {
-	_, err := dictionary.LoadDefault("actors")
-
-	assert.EqualError(t, err, "unable to load dictionary `assets/actors.txt")
+	// YOUR_CODE...
 }
 
 func TestLoadCustomFail(t *testing.T) {
-	_, err := dictionary.Load("zorg", "actors")
-	fmt.Println(err)
-	assert.EqualError(t, err, "unable to load dictionary `zorg/actors.txt")
+	// YOUR_CODE...
 }
