3dd4513e4c7889698dbf1939980b47b5f845d0a4
derailed <fernand.galiana@gmail.com>
2018-02-10 18:48:13 -0700

    updates

--- fizzbuzz/game_test.go
+++ fizzbuzz/game_test.go
@@ -1,7 +1,6 @@
 package fizzbuzz_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/imhotepio/letsgo_labs/fizzbuzz"
@@ -21,25 +20,4 @@ func TestCompute(t *testing.T) {
 	for k, v := range uc {
 		assert.Equal(t, v, fizzbuzz.Compute(k))
 	}
-}
-
-// Returns `FizzBuzz if number is div by 3 and 5
-func ExampleCompute_fizzbuzz() {
-	fmt.Println(fizzbuzz.Compute(15))
-	// Output:
-	// FizzBuzz
-}
-
-// Returns `Fizz if number is div by 3
-func ExampleCompute_fizz() {
-	fmt.Println(fizzbuzz.Compute(3))
-	// Output:
-	// Fizz
-}
-
-// Returns `Buzz if number is div by 5
-func ExampleCompute_buzz() {
-	fmt.Println(fizzbuzz.Compute(5))
-	// Output:
-	// Buzz
 }
