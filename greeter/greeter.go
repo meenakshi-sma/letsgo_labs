ab61b1ac26a711d578eadd791807fed8f1102e6c
derailed <fernand.galiana@gmail.com>
2018-05-05 12:27:34 -0600

    updates

--- greeter/greeter.go
+++ greeter/greeter.go
@@ -5,6 +5,7 @@ package greeter
 import (
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 const greeting = "Hello, %s! You are %d old today..."
@@ -17,4 +18,26 @@ func greeter(n string, a int) string {
 // greeter1 return a greeting based on name, age and height
 func greeter1(n string, a int) string {
 	return "Hello, " + n + "! You are " + strconv.Itoa(a) + " old today..."
+}
+
+func greeter2(n string, a int) string {
+	var b strings.Builder
+
+	b.Grow(39)
+	// fmt.Fprintf(&b, "Hello, "+n+"! You are "+strconv.Itoa(a)+" old today...")
+	b.WriteString("Hello, ")
+	b.WriteString(n)
+	b.WriteString("! You are ")
+	b.WriteString(strconv.Itoa(a))
+	b.WriteString(" old today...")
+
+	return b.String()
+}
+
+func greeter3(n string, a int) string {
+	var buff = make([]byte, 39)
+
+	copy(buff, "Hello, "+n+"! You are "+strconv.Itoa(a)+" old today...")
+
+	return string(buff)
 }
--- greeter/greeter_test.go
+++ greeter/greeter_test.go
@@ -19,6 +19,18 @@ func TestGreeter1(t *testing.T) {
 	assert.Equal(t, fmt.Sprintf(greeting, n, a), greeter1(n, a))
 }
 
+func TestGreeter2(t *testing.T) {
+	n, a := "Fernand", 30
+
+	assert.Equal(t, fmt.Sprintf(greeting, n, a), greeter2(n, a))
+}
+
+func TestGreeter3(t *testing.T) {
+	n, a := "Fernand", 30
+
+	assert.Equal(t, fmt.Sprintf(greeting, n, a), greeter3(n, a))
+}
+
 func BenchmarkGreeter(b *testing.B) {
 	n, a := "Fernand", 30
 
@@ -36,5 +48,25 @@ func BenchmarkGreeter1(b *testing.B) {
 
 	for i := 0; i < b.N; i++ {
 		_ = greeter1(n, a)
+	}
+}
+
+func BenchmarkGreeter2(b *testing.B) {
+	n, a := "Fernand", 30
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_ = greeter2(n, a)
+	}
+}
+
+func BenchmarkGreeter3(b *testing.B) {
+	n, a := "Fernand", 30
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_ = greeter3(n, a)
 	}
 }
