7f41f4ed9214615de5aa054153325fee5b73377f
derailed <fernand.galiana@gmail.com>
2018-05-06 09:47:36 -0600

    updates

--- greeter/greeter.go
+++ greeter/greeter.go
@@ -10,11 +10,11 @@ import (
 const greeting = "Hello, %s! You are %d old today..."
 
 // greeter return a greeting based on name, age and height
-func greeter(n string, a int) string {
+func greeter1(n string, a int) string {
 	return fmt.Sprintf(greeting, n, a)
 }
 
 // greeter1 return a greeting based on name, age and height
-func greeter1(n string, a int) string {
+func greeter2(n string, a int) string {
 	return "Hello, " + n + "! You are " + strconv.Itoa(a) + " old today..."
 }
--- greeter/greeter_test.go
+++ greeter/greeter_test.go
@@ -7,32 +7,30 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGreeter(t *testing.T) {
-	n, a := "Fernand", 30
-
-	assert.Equal(t, fmt.Sprintf(greeting, n, a), greeter(n, a))
-}
-
 func TestGreeter1(t *testing.T) {
 	n, a := "Fernand", 30
 
 	assert.Equal(t, fmt.Sprintf(greeting, n, a), greeter1(n, a))
 }
 
-func BenchmarkGreeter(b *testing.B) {
+func TestGreeter2(t *testing.T) {
 	n, a := "Fernand", 30
-	b.ResetTimer()
-	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
-		_ = greeter(n, a)
-	}
+
+	assert.Equal(t, fmt.Sprintf(greeting, n, a), greeter2(n, a))
 }
 
 func BenchmarkGreeter1(b *testing.B) {
 	n, a := "Fernand", 30
 	b.ResetTimer()
-	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
-		_ = greeter1(n, a)
+		greeter1(n, a)
+	}
+}
+
+func BenchmarkGreeter2(b *testing.B) {
+	n, a := "Fernand", 30
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		greeter2(n, a)
 	}
 }
