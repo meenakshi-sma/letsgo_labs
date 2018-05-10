ed174281377a83454790ae22838c01d73d9b7c86
derailed <fernand.galiana@gmail.com>
2018-05-10 06:41:59 -0400

    updates

---
+++ errors/README.md
@@ -0,0 +1,33 @@
+<img src="../assets/gophernand.png" align="right" width="100" height="auto"/>
+
+<br/>
+<br/>
+<br/>
+
+# LetsGo! Errors FizzBuzz Lab...
+
+## Mission
+
+> Enhance The FizzBuzz game!
+
+* Write a program that iterates from 0 to 21 included
+* The program should issue an error if the number is <= 0 or > 20
+* Otherwise the program should print the fizzbuzz representation of that number.
+
+## Installation
+
+```shell
+# Install errors package
+go get -f -u github.com/pkg/errors
+```
+
+
+## Up And Running
+
+```shell
+go run game.go
+```
+
+---
+<img src="../assets/imhotep_logo.png" width="32" height="auto"/> © 2018 Imhotep Software LLC.
+All materials licensed under [Apache v2.0](http://www.apache.org/licenses/LICENSE-2.0)
---
+++ errors/game.go
@@ -0,0 +1,47 @@
+// Enhance The FizzBuzz game!
+// o Write a program that iterates from 0 to 21 included
+// o The program should issue an error if the number is <= 0 or > 20
+// o Otherwise the program should print the fizzbuzz representation of that number.
+package main
+
+import (
+	"fmt"
+
+	"github.com/pkg/errors"
+)
+
+const (
+	fizz     = "Fizz"
+	buzz     = "Buzz"
+	fizzBuzz = fizz + buzz
+)
+
+// Define errors...
+// YOUR_CODE...
+
+
+func main() {
+	for i := 0; i <= 21; i++ {
+		fmt.Printf("%02d ", i)
+		if r, err := play(i); err != nil {
+			fmt.Printf("%v\n", err)
+		} else {
+			fmt.Printf("%v\n", r)
+		}
+	}
+}
+
+func play(n int) // YOUR_CODE {
+  // YOUR_CODE...
+	switch {
+	case n%3 == 0 && n%5 == 0:
+		s = fizzBuzz
+	case n%3 == 0:
+		s = fizz
+	case n%5 == 0:
+		s = buzz
+	default:
+		s = fmt.Sprintf("%d", n)
+	}
+	// YOUR_CODE...
+}
