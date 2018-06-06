def1a89bb1d2fdad6ae0f90c13e39d00cf6298c5
derailed <fernand.galiana@gmail.com>
2018-06-06 11:04:17 -0600

    updates

---
+++ hangman/README.md
@@ -0,0 +1,23 @@
+<img src="../assets/gophernand.png" align="right" width="100" height="auto"/>
+
+<br/>
+<br/>
+<br/>
+
+# LetsGo! Hangman Lab
+
+---
+## <img src="../assets/lab.png" width="auto" height="32"/> Mission
+
+> Finish the implementation of Hangman
+
+* Implement the Guess method in game.go leveraging the helper functions
+* Leveraging table tests ensure your implementation is correct
+* Time to enjoy your work! By all means play the game:
+    ```shell
+    go run cmd/cli.go
+    ```
+
+---
+<img src="../assets/imhotep_logo.png" width="32" height="auto"/> © 2018 Imhotep Software LLC.
+All materials licensed under [Apache v2.0](http://www.apache.org/licenses/LICENSE-2.0)
--- hangman/game.go
+++ hangman/game.go
@@ -1,7 +1,6 @@
 package hangman
 
 import (
-	"fmt"
 	"math/rand"
 	"time"
 )
@@ -34,33 +33,7 @@ func NewGame(path string) (g *Game, err error) {
 
 // Guess a letter in the game
 func (g *Game) Guess(r rune) (*Tally, error) {
-	if g.tally.Status == Won || g.tally.Status == Lost {
-		return g.tally, fmt.Errorf("game over")
-	}
-
-	if inRunes(g.guesses, r) {
-		g.tally.Status = AlreadyGuessed
-		return g.tally, nil
-	}
-
-	g.guesses = append(g.guesses, r)
-	g.tally.Letters = lettersFromGuess(g.word, g.guesses)
-
-	if inRunes([]rune(g.word), r) {
-		g.tally.Status = CorrectGuess
-		if !missingLetters(g.tally.Letters) {
-			g.tally.Status = Won
-		}
-		return g.tally, nil
-	}
-
-	g.tally.TurnsLeft--
-	g.tally.Status = IncorrectGuess
-	if g.tally.TurnsLeft == 0 {
-		g.tally.Status = Lost
-		g.tally.Word = g.word
-	}
-	return g.tally, nil
+	// YOUR_CODE...
 }
 
 func lettersFromGuess(w string, guesses []rune) []rune {
