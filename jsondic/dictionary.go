10e1b9ffaec27d478c8b2e0a96781387f5eae61c
derailed <fernand.galiana@gmail.com>
2018-06-12 21:30:29 -0700

    updates

--- jsondic/dictionary.go
+++ jsondic/dictionary.go
@@ -18,17 +18,14 @@ type Entry struct {
 // Encode an entry into JSON
 func Encode(e Entry) (string, error) {
 	var buff bytes.Buffer
-	err := json.NewEncoder(&buff).Encode(e)
-	if err != nil {
+	if err := json.NewEncoder(&buff).Encode(e); err != nil {
 		return "", err
 	}
-
 	return buff.String(), nil
 }
 
 // Decode hydrates an entry from JSON
 func Decode(raw string) (e Entry, err error) {
 	err = json.NewDecoder(bytes.NewReader([]byte(raw))).Decode(&e)
-
 	return
 }
